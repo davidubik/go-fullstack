91ccd94b9325a5fdc90ad7eb7159ae27cd1f7944
davidubik <dtwebdev@outlook.fr.com>
2021-08-26 22:34:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+const server =  http.createServer(app);
+
+app.set('port',process.env.PORT || 3000);
+
+server.listen(process.env.PORT || 3000);
