f5f03f82f697bbe64867a9030d650ef007dfaa0d
davidubik <dtwebdev@outlook.fr.com>
2021-09-09 23:19:47 +0200

    Création des Roots pour modifier & supprimé un objet dans l application, Désormais, votre application implémente le CRUD complet

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
   app.use(bodyParser.json());
 
-  //ROOT POST
+  //ROOT POST CREATION D'UN NOUVELLE OBJET
   app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
@@ -36,8 +36,23 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
-// Root dynamque pour récupéré un Objet via son Id
+//ROOT PUT pour modifier un Objet 
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id}) // Modification d'un objet on l'identifie par son Id et le deuxième argument est l'id du produit modifier
+    .then(() => res.status(200).json({ message : 'Objet Modifier !'}))
+    .catch(error => res.status(400).json({ error })); 
+});
+
+//Suppression d'un Objet (route DELETE)
+app.delete('/api/stuff/:id', (req, res, next)=>{
+  Thing.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({ message : 'Objet Supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
+// Root dynamque pour récupéré un Objet via son Id
 app.get('/api/stuff/:id' , (req, res, next) =>{
   Thing.findOne({_id: req.params.id}) // trouve l'objet par sont Id
     .then(thing => res.status(200).json(thing))
