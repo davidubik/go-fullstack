6e63245ce69d6e1cca7a0490368c4cc562d9a1ad
davidubik <dtwebdev@outlook.fr.com>
2021-09-09 21:46:05 +0200

    Changement de logique sur le middileware app.get qui permet d enregistré un nouvelle objet pour la vente ainsi qui implémentation de récupéré un objet par son Id et l afficher dynamiquement dans le Front

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const things = require('./models/thing');
+const Thing = require('./models/thing');
+const thing = require('./models/thing');
 
-mongoose.connect('mongodb+srv://David:Bladerunner2049@cluster0.5ekqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;',
+mongoose.connect('mongodb+srv://David:Bladerunner2049@cluster0.5ekqj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -35,26 +36,18 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+// Root dynamque pour récupéré un Objet via son Id
+
+app.get('/api/stuff/:id' , (req, res, next) =>{
+  Thing.findOne({_id: req.params.id}) // trouve l'objet par sont Id
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+   Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
   });
 
 module.exports = app;
