ffc21f02c54387168dbb5e4c445ee02e5be59ebf
davidubik <dtwebdev@outlook.fr.com>
2021-09-09 21:42:45 +0200

    Correction du bug qui venait d une faute de frappe sur imageUrl

--- models/Thing.js
+++ models/Thing.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const thingSchema = mongoose.Schema({
     title:{type :String, required : true},
     description : {type : String, required : true},
-    imgaheUrl : {type : String, required : true},
+    imageUrl : {type : String, required : true},
     userId : {type : String, required : true},
     price : {type : Number, required : true}
 });
